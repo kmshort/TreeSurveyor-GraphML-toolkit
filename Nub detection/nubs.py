5791ec995784d6ce847c98e2bd7274b9668c4df4
Kieran Short <kieran.short@monash.edu>
2019-04-11 17:40:54 +1000

    remove superfluous functions
    some hangovers were still in here when I cannibalised another script. Removed them to clean it up.

--- Nub detection/nubs.py
+++ Nub detection/nubs.py
@@ -1,10 +1,15 @@
 import networkx as nx
-import numpy as np
 import argparse
 import tree_routines_X3 as tR
 import csv
 
 def main():
+'''
+A rough tool for detecting tips that are internally located. Basically, it checks its neighbour, and sees how many derivatives there are.. if it's a lot, then chances are it's an internal tip.
+This will export a csv file, with a list of Tree Surveyor BranchIDs. Next to it, a number of derivatives.
+Use this with Tree Surveyor's "Skeleton Curation", enable "Corrected segment data" visualisation. Hit the "HOME" key, and enter in the BranchIDs with lots of derivatives. Then, manually curate the tip.
+'''
+
     parser = argparse.ArgumentParser(prog='TipSeg', formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('graph_file')
     args = parser.parse_args()
@@ -30,7 +35,4 @@ def main():
             branchdata = [tip, t]
             dataWriter.writerow(branchdata)
             
-            # write raw to minc converter line for Mattijs
-            # convert um dimensions to millimetre, used by rawtominc in linux
-    tR.plotKidneyHull(nxGraphML)
 main()
